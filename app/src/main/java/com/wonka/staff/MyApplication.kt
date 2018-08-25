e9dea46e1ef933d52aca634df6d078e64b678473
David Llorca <david.llorca@mango.com>
2018-08-25 18:18:59 +0200

    Set up Android injector by dagger.android

--- app/src/main/java/com/wonka/staff/MyApplication.kt
+++ app/src/main/java/com/wonka/staff/MyApplication.kt
@@ -1,22 +1,24 @@
 package com.wonka.staff
 
+import android.app.Activity
 import android.app.Application
-import com.wonka.staff.common.ApplicationComponent
-import com.wonka.staff.common.ApplicationModule
 import com.wonka.staff.common.DaggerApplicationComponent
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasActivityInjector
 
 
-class MyApplication : Application() {
+class MyApplication : Application(), HasActivityInjector {
 
-    private lateinit var mApplicationComponent: ApplicationComponent
+    private lateinit var mActivityInjector: DispatchingAndroidInjector<Activity>
 
     override fun onCreate() {
         super.onCreate()
-        mApplicationComponent = DaggerApplicationComponent.builder()
-                .applicationModule(ApplicationModule(this))
+        DaggerApplicationComponent.builder()
+                .application(this)
                 .build()
+                .inject(this)
     }
 
-    fun getApplicationComponent(): ApplicationComponent = mApplicationComponent
-
+    override fun activityInjector(): AndroidInjector<Activity> = mActivityInjector
 }
--- app/src/main/java/com/wonka/staff/common/ApplicationComponent.kt
+++ app/src/main/java/com/wonka/staff/common/ApplicationComponent.kt
@@ -1,11 +1,28 @@
 package com.wonka.staff.common
 
+import android.app.Application
+import com.wonka.staff.MyApplication
+import dagger.BindsInstance
 import dagger.Component
+import dagger.android.AndroidInjectionModule
 import javax.inject.Singleton
 
 @Singleton
 @Component(modules = [
     (ApplicationModule::class),
     (NetworkingModule::class),
-    (WonkaApiModule::class)])
-interface ApplicationComponent
+    (WonkaApiModule::class),
+    (AndroidInjectionModule::class)])
+interface ApplicationComponent {
+
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        fun application(application: Application): Builder
+
+        fun build(): ApplicationComponent
+    }
+
+    fun inject(myApplication: MyApplication)
+
+}
